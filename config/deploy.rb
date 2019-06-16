b73aee833f1ec693287fa4fe638d33ef480342ba
Martin Schaflitzl <martin@martin-sc.de>
2019-06-16 22:18:49 +0200

    fix linked database

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,6 @@ set :repo_url, "gitlab@git.martin-sc.de:martin/home_automation.git"
 set :scm_user, "deploy"
 server "home.ramaschaf.de", user: "deploy-home_automation", roles: %w{app db web}
 set :use_sudo, false
-set :linked_files, ['config/master.key', 'config/database,yml']
+set :linked_files, ['config/master.key', 'config/database.yml']
 set :rails_env, "production"
 set :keep_releases, 5
